1e2d775fb1673779831744be01e2a8c06a6fd959
Klaus Boeing <klausboeing@gmail.com>
2014-09-19 22:39:55 -0300

    ajustado pacotes

--- src/teste/jdbc/ConnectionFactory.java
+++ src/teste/jdbc/ConnectionFactory.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package jdbc.exemplos;
+package teste.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
